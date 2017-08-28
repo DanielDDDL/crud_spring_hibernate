520bab2d3845350bd4188fcff90a60ff3fe8d759
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-08-28 18:12:04 -0300

    initial controller

--- src/main/java/br/mackenzie/lfs/crud_spring_hibernate/controllers/LinkController.java
+++ src/main/java/br/mackenzie/lfs/crud_spring_hibernate/controllers/LinkController.java
@@ -5,6 +5,12 @@
  */
 package br.mackenzie.lfs.crud_spring_hibernate.controllers;
 
+import br.mackenzie.lfs.crud_spring_hibernate.model.Book;
+import br.mackenzie.lfs.crud_spring_hibernate.model.Tag;
+import br.mackenzie.lfs.crud_spring_hibernate.services.BookService;
+import br.mackenzie.lfs.crud_spring_hibernate.services.TagService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,14 +22,33 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LinkController {
     
+    @Autowired
+    private TagService tagService;
+    
+    @Autowired
+    private BookService bookService;
+    
     @RequestMapping(value="/")
     public ModelAndView mainPage(){
-        return new ModelAndView("home");
+        return gettingInitialInfo();
     }
     
     @RequestMapping(value="/index")
     public ModelAndView indexPage(){
-        return new ModelAndView("home");
+        return gettingInitialInfo();
+    }
+    
+    private ModelAndView gettingInitialInfo(){
+        
+        List<Tag> tags = tagService.getTags();
+        List<Book> books = bookService.getBooks();
+        
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("books", books);
+        modelAndView.addObject("tags", tags);
+        
+        return modelAndView;
+        
     }
     
 }
