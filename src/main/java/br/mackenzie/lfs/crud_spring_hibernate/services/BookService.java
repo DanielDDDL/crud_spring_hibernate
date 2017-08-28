2e353301e026437cbc53d313b9f2e8c7b17e44d3
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-08-28 02:48:51 -0300

    service classes - their interfaces and implementations

---
+++ src/main/java/br/mackenzie/lfs/crud_spring_hibernate/services/BookService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.mackenzie.lfs.crud_spring_hibernate.services;
+
+import br.mackenzie.lfs.crud_spring_hibernate.model.Book;
+import java.util.List;
+
+/**
+ *
+ * @author Dias
+ */
+public interface BookService {
+    
+    public void addBook(Book book);
+    public void updateBook(Book book);
+    public Book getBook(int id);
+    public void deleteBook(int id);
+    public List<Book> getBooks();
+
+}
