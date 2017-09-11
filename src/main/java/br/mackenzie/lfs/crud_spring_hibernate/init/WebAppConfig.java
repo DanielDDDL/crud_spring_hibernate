d58c2295556e2878ead105536b357a89ee442e90
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-09-11 17:00:30 -0300

    connecting through jndi

--- src/main/java/br/mackenzie/lfs/crud_spring_hibernate/init/WebAppConfig.java
+++ src/main/java/br/mackenzie/lfs/crud_spring_hibernate/init/WebAppConfig.java
@@ -6,6 +6,9 @@
 package br.mackenzie.lfs.crud_spring_hibernate.init;
 
 import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -48,7 +53,9 @@ public class WebAppConfig {
     private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
     private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
     private static final String PROPERTY_NAME_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
-    
+
+    private static final String PROPERTY_NAME_JNDI = "database.jndi";
+
     @Autowired
     private Environment env;
     
@@ -62,14 +69,11 @@ public class WebAppConfig {
     }
     
     @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-         
-        dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
-        dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
-        dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
-        dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
-         
+    public DataSource dataSource() {
+
+        JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+        dsLookup.setResourceRef(true);
+        DataSource dataSource = dsLookup.getDataSource(env.getProperty(PROPERTY_NAME_JNDI));
         return dataSource;
     }
     
