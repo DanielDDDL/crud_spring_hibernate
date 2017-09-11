f0f4772cdbb996cc7db28212f957ee142289ed97
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-09-11 18:16:33 -0300

    removing unnecessry things - in general

--- src/main/java/br/mackenzie/lfs/crud_spring_hibernate/controllers/BookController.java
+++ src/main/java/br/mackenzie/lfs/crud_spring_hibernate/controllers/BookController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.mackenzie.lfs.crud_spring_hibernate.controllers;
 
 import br.mackenzie.lfs.crud_spring_hibernate.model.Book;
@@ -10,20 +6,14 @@ import br.mackenzie.lfs.crud_spring_hibernate.model.Tag;
 import br.mackenzie.lfs.crud_spring_hibernate.model.request.TagPropertyEditor;
 import br.mackenzie.lfs.crud_spring_hibernate.services.BookService;
 import br.mackenzie.lfs.crud_spring_hibernate.services.TagService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  *
--- src/main/java/br/mackenzie/lfs/crud_spring_hibernate/model/Book.java
+++ src/main/java/br/mackenzie/lfs/crud_spring_hibernate/model/Book.java
@@ -1,27 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.mackenzie.lfs.crud_spring_hibernate.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 /**
  *
